e66df8bb83d992822f1ff3c4d9e36b5b18c1c3dd
Carolyn Nguyen <CarolynNguyen@Carolyns-MacBook-Pro.local>
2014-07-23 23:34:42 -0700

    completed min requirements

--- script.js
+++ script.js
@@ -6,33 +6,41 @@ gameApp.controller("ticController", function($scope){
 var board;
 var checkWin;
 var reset;
+var endGame = false;
+// var result;
+
+$scope.resultOne = ' ';
+
+
 
 $scope.board = [" "," "," "," "," "," "," "," "," "];
-
+$scope.result = true;
 $scope.clicked = 0
  
 $scope.clicker = function(b) {
 
-	if ($scope.board[b] == " ") {
-
+	if ($scope.board[b] == " " && endGame == false){
 		$scope.clicked+=1;
 		// console.log($scope.clicked);
   		if ($scope.clicked % 2 == 0) { 
-			$scope.board[b] = 'O';
-			checkWin();
+  			$scope.board[b] = 'O';
 		} 
 		else {
 			$scope.board[b] = 'X';
-			checkWin();
-		}
-	}
+		} 
+	};
+	
+	checkWin();
+};
 
-};	
 $scope.reset = function(b){
-		// $scope.game.board = $scope.initial;		
-		$scope.board = [" "," "," "," "," "," "," "," "," "];
-		$scope.clicked = 0;
-		$scope.gameWin = false;
+	location.reload(true);
+// 		// $scope.game.board = $scope.initial;		
+// 		$scope.board = [" "," "," "," "," "," "," "," "," "];
+// 		$scope.clicked = 0;
+// 		$scope.gameWin = false;
+
+		
 
 };
 
@@ -40,83 +48,57 @@ var checkWin = function() {
 	if ($scope.board[0] == $scope.board[1] && 
 		$scope.board[2] == $scope.board[1] &&
 		$scope.board[1] !== " ") {
-			if ($scope.board[0] == 'X') {
-				alert("X wins!");
-			}else {
-				alert("O wins!");
-			}
-	}
+		endGame = true; 
+		$scope.resultOne = 'Winner!';
+		console.log(endGame);
+			console.log(result);
 
+	}
 	if ($scope.board[3] == $scope.board[4] &&
 		$scope.board[5] == $scope.board[4] &&
 		$scope.board[5] !== " ") {
-			if ($scope.board[3] == 'X') {
-				alert("X wins");
-			} else {
-				alert("O wins!");
-			}
+		endGame = true;
+		$scope.resultOne = 'Winner!';
 	}
-
 	if ($scope.board[6] == $scope.board[7] &&
 		$scope.board[8] == $scope.board[7] &&
 		$scope.board[8] !== " ") {
-			if ($scope.board[6] =='X') {
-				alert("X wins!");
-			} else {
-				alert("O wins!");
-			}
-
+		endGame = true;
+		$scope.resultOne = 'Winner!';
 	}
-
 	if ($scope.board[0] == $scope.board[3] &&
 		$scope.board[6] == $scope.board[3] &&
-		$scope.board[6] !==" "){
-			if ($scope.board[0] == 'X') {
-				alert("X wins!");
-			} else {
-				alert("O wins!");
-			}
+		$scope.board[6] !==" ") {
+		endGame = true;
+		$scope.resultOne = 'Winner!';
 	}
-
 	if ($scope.board[1] == $scope.board[4] && 
-		$scope.board[7]== $scope.board[4] && 
-		$scope.board[7]!==" "){
-			if ($scope.board[1] == 'X') {
-				alert("X wins!");
-			} else {
-				alert("O wins!");
-			}
-		}
-		if ($scope.board[2] == $scope.board[5] && 
-			$scope.board[8]== $scope.board[5] && 
-			$scope.board[8]!==" "){
-			if ($scope.board[2] == 'X') {
-				alert("X wins!");
-			} else {
-				alert("O wins!");
-			}
-		}
-		if ($scope.board[0] == $scope.board[4] && 
-			$scope.board[8]== $scope.board[4] && 
-			$scope.board[8]!==" "){
-			if ($scope.board[0] == 'X') {
-				alert("X wins!");				
-			} else {
-				alert("O wins!");
-			}		
-		}
-		if ($scope.board[2] == $scope.board[4] && 
-			$scope.board[6]== $scope.board[4] && 
-			$scope.board[6]!==" "){
-			if ($scope.board[2] == 'X') {
-				alert("X wins!");				
-			} else {
-				alert("O wins!");				
-			}
-		}	
-		if ($scope.clicked === 9 && !$scope.checkWin) {
-			alert("Draw!");
-		}
+		$scope.board[7] == $scope.board[4] && 
+		$scope.board[7] !==" ") {
+		endGame = true;
+		$scope.resultOne = 'Winner!';
+	}
+	if ($scope.board[2] == $scope.board[5] && 
+		$scope.board[8] == $scope.board[5] && 
+		$scope.board[8] !==" ") {
+		endGame = true;
+		$scope.resultOne = 'Winner!';
+	}
+	if ($scope.board[0] == $scope.board[4] && 
+		$scope.board[8] == $scope.board[4] && 
+		$scope.board[8] !==" "){
+		endGame = true;
+		$scope.resultOne = 'Winner!';
+	}
+	if ($scope.board[2] == $scope.board[4] && 
+		$scope.board[6] == $scope.board[4] && 
+		$scope.board[6] !==" "){
+		endGame = true;
+		$scope.resultOne = 'Winner!';
+	}	
+	if ($scope.clicked === 9 && !$scope.checkWin) {
+		$scope.resultOne = "It's a Tie";
+	}
 };
 
 
